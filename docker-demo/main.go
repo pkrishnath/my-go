ca477d8a5a8852cde688d9ec6e2d00ed440910e2
unknown <poolok01@BC67786.redbeemedia.net>
2017-01-28 22:24:56 +0000

    fixed compilation error

--- docker-demo/main.go
+++ docker-demo/main.go
@@ -5,14 +5,11 @@ import (
     "html"
     "log"
     "net/http"
-     "github.com/Sirupsen/logrus"
-
 )
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-        log.
     })
 
     log.Fatal(http.ListenAndServe(":8080", nil))
